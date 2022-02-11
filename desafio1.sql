f37ebef1d5b0f17f3d118ee361cbe9ce25b46cc6
Miguel_Reis <guelsouzar@gmail.com>
2022-02-11 02:18:12 -0300

    desafio 1 de novo

--- desafio1.sql
+++ desafio1.sql
@@ -5,45 +5,46 @@ CREATE DATABASE SpotifyClone;
 CREATE TABLE SpotifyClone.usuarios(
     usario_id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50),
-    idade INT,
-    plano_id INT,
-    data_da_assinatura DATE,
+    idade INT NOT NULL,
+    plano_id INT NOT NULL,
+    data_da_assinatura DATE NOT NULL,
     FOREIGN KEY (plano_id) REFERENCES economia(plano_id)
 ) engine = InnoDB;
 CREATE TABLE SpotifyClone.economia(
     plano_id INT PRIMARY KEY AUTO_INCREMENT,
-    plano VARCHAR(50),
-    valor_do_plano DECIMAL (3,2)
+    plano VARCHAR(20)NOT NULL,
+    valor_do_plano DECIMAL(3,2) NOT NULL
 ) engine = InnoDB;
 CREATE TABLE SpotifyClone.artistas(
     artista_id INT PRIMARY KEY AUTO_INCREMENT,
-    artista VARCHAR(50)
+    artista VARCHAR(50) NOT NULL
 ) engine = InnoDB;
 CREATE TABLE SpotifyClone.albuns(
     albuns_id INT PRIMARY KEY AUTO_INCREMENT,
-    albuns VARCHAR(50),
-    artista_id INT,
-    ano_de_lancamento year,
+    albuns VARCHAR(50) NOT NULL,
+    artista_id INT NOT NULL,
+    ano_de_lancamento year NOT NULL,
     FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) engine = InnoDB;
 CREATE TABLE SpotifyClone.musicas(
     cancoes_id INT PRIMARY KEY AUTO_INCREMENT,
-    cancoes VARCHAR(50),
-    albuns_id INT,
-    duracao_seguntos time,
+    cancoes VARCHAR(50) NOT NULL,
+    albuns_id INT NOT NULL,
+    duracao_seguntos time NOT NULL,
     FOREIGN KEY (album_id) REFERENCES albuns(albuns_id)
 ) engine = InnoDB;
 CREATE TABLE SpotifyClone.artistas_seguidos(
-    usario_id INT PRIMARY KEY,
-    artista_id INT PRIMARY KEY,
+    usario_id INT PRIMARY KEY NOT NULL,
+    artista_id INT PRIMARY KEY NOT NULL,
     FOREIGN KEY (usario_id) REFERENCES usuarios(usario_id),
     FOREIGN KEY (artista_id) REFERENCES artista(artista_id)
 ) engine = InnoDB;
 CREATE TABLE SpotifyClone.historicos(
-    usario_id INT PRIMARY KEY,
-    historico_de_reproducoes VARCHAR(50) PRIMARY KEY,
-    data_reproducao datetime,
-    FOREIGN KEY (usario_id) REFERENCES usuarios(usario_id)
+    usario_id INT  PRIMARY KEY NOT NULL,
+    cancoes_id INT PRIMARY KEY NOT NULL,
+    data_reproducao datetime NOT NULL,
+    FOREIGN KEY (usario_id) REFERENCES usuarios(usario_id),
+    FOREIGN KEY (cancoes_id) REFERENCES musicas(cancoes_id)
 ) engine = InnoDB;
 
 INSERT INTO SpotifyClone.economia (plano, valor_do_plano)
@@ -156,46 +157,46 @@ VALUES
   (10,2),
   (10,5);
 
-INSERT INTO SpotifyClone.historicos (usario_id, historico_de_reproducoes, data_reproducao)
+INSERT INTO SpotifyClone.historicos (usario_id, cancoes_id, data_reproducao)
 VALUES
-  (1,"Honey" ,'2020-02-28 10:45:55'),
-  (1,"Walking And Man" ,'2020-05-02 05:30:35'),
-  (1,"Young And Father" ,'2020-03-06 11:22:33'),
-  (1,"Diamond Power" ,'2020-08-05 08:05:17'),
-  (1,"Let's Be Silly" ,'2020-09-14 16:32:22'),
-  (2,"I Heard I Want To Bo Alone" ,'2020-01-02 07:40:33'),
-  (2,"Finding My Traditions" ,'2020-01-02 07:40:33'),
-  (2,"Without My Love" ,'2020-10-09 12:27:48'),
-  (2,"Baby" ,'2020-09-21 13:14:46'),
-  (3,"Magic Circus" ,'2020-11-13 16:55:13'),
-  (3,"Dance With Her Own" ,'2020-12-05 18:38:30'),
-  (3,"Hard And Time" ,'2020-07-30 10:00:00'),
-  (4,"Reflections Of Magic" ,'2021-08-15 17:10:10'),
-  (4,"I Ride Alone" ,'2021-07-10 15:20:30'),
-  (4,"Honey, I'm A Lone Wolf" ,'2021-01-09 01:44:33'),
-  (5,"Honey, So Do I" ,'2020-07-03 19:33:28'),
-  (5,"Rock His Everything" ,'2017-02-24 21:14:22'),
-  (5,"Diamond Power" ,'2020-08-06 15:23:43'),
-  (5,"Soul For Us" ,'2020-11-10 13:52:27'),
-  (6,"Wouldn't It Be Nice" ,'2019-02-07 20:33:48'),
-  (6,"He Heard You're Bad For Me" ,'2017-01-24 00:31:17'),
-  (6,"He Hopes We Can't Stay" ,'2017-10-12 12:35:20'),
-  (6,"Walking And Game" ,'2018-05-29 14:56:41'),
-  (7,"Time Fireworks" ,'2018-05-09 22:30:49'),
-  (7,"Troubles Of My Inner Fire" ,'2020-07-27 12:52:58'),
-  (7,"Celebration Of More" ,'"2018-01-16 18:40:43'),
-  (8,"Baby" ,'2018-03-21 16:56:40'),
-  (8,"You Make Me Feel So.." ,'2020-10-18 13:38:05'),
-  (8,"He's Walking Away" ,'2019-05-25 08:14:03'),
-  (8,"He's Trouble" ,'2021-08-15 21:37:09'),
-  (9,"Thang Of Thunder" ,'2021-05-24 17:23:45'),
-  (9,"Words Of Her Life" ,'2018-12-07 22:48:52'),
-  (9,"Sweetie, Let's Go Wild" ,'2021-03-14 06:14:26'),
-  (9,"She Knows" ,'2020-04-01 03:36:00'),
-  (10,"History Of My Roses" ,'2017-02-06 08:21:34'),
-  (10,"Without My Love" ,'2017-12-04 05:33:43'),
-  (10,"Rock His Everything" ,'2017-07-27 05:24:49'),
-  (10,"Home Forever" ,'2017-12-25 01:03:57');
+  (1,36 ,'2020-02-28 10:45:55'),
+  (1,25 ,'2020-05-02 05:30:35'),
+  (1,23 ,'2020-03-06 11:22:33'),
+  (1,14 ,'2020-08-05 08:05:17'),
+  (1,15 ,'2020-09-14 16:32:22'),
+  (2,34 ,'2020-01-02 07:40:33'),
+  (2,24 ,'2020-01-02 07:40:33'),
+  (2,21 ,'2020-10-09 12:27:48'),
+  (2,39 ,'2020-09-21 13:14:46'),
+  (3,6 ,'2020-11-13 16:55:13'),
+  (3,3 ,'2020-12-05 18:38:30'),
+  (3,26 ,'2020-07-30 10:00:00'),
+  (4,2 ,'2021-08-15 17:10:10'),
+  (4,35 ,'2021-07-10 15:20:30'),
+  (4,27 ,'2021-01-09 01:44:33'),
+  (5,7 ,'2020-07-03 19:33:28'),
+  (5,12 ,'2017-02-24 21:14:22'),
+  (5,14 ,'2020-08-06 15:23:43'),
+  (5,1 ,'2020-11-10 13:52:27'),
+  (6,38 ,'2019-02-07 20:33:48'),
+  (6,29 ,'2017-01-24 00:31:17'),
+  (6,30 ,'2017-10-12 12:35:20'),
+  (6,22 ,'2018-05-29 14:56:41'),
+  (7,5 ,'2018-05-09 22:30:49'),
+  (7,4 ,'2020-07-27 12:52:58'),
+  (7,11 ,'"2018-01-16 18:40:43'),
+  (8,39 ,'2018-03-21 16:56:40'),
+  (8,40 ,'2020-10-18 13:38:05'),
+  (8,32 ,'2019-05-25 08:14:03'),
+  (8,33 ,'2021-08-15 21:37:09'),
+  (9,16 ,'2021-05-24 17:23:45'),
+  (9,17 ,'2018-12-07 22:48:52'),
+  (9,8 ,'2021-03-14 06:14:26'),
+  (9,9 ,'2020-04-01 03:36:00'),
+  (10,20 ,'2017-02-06 08:21:34'),
+  (10,21 ,'2017-12-04 05:33:43'),
+  (10,12,'2017-07-27 05:24:49'),
+  (10,13 ,'2017-12-25 01:03:57');
   
   -- FIQUEI UM POUCO PERDIDO COM A MONTAGEM DE TABELA ENTÃO ME BASEI NA TABELA DO Carlos lima 
   -- https://github.com/tryber/sd-015-b-mysql-one-for-all/pull/111/commits/b75a8554bbf6b0f54c74d5651509f8852a8c7c48
