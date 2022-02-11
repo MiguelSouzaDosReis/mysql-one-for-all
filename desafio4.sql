e3e89ff7d4a1c4dc93262528e83abefba2be32e7
Miguel_Reis <guelsouzar@gmail.com>
2022-02-11 09:18:45 -0300

    desafio 4 de novo

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,6 @@
  select u.nome as usuario,
- IF (YEAR(MAX(h.data_reproducao)) >= 2021, 'Usuario ativo', 'Usuario inativo') AS condicao_usuaria
+ IF (MAX(h.data_reproducao) >= '2021-01-01 00:00:00'
+ AND MAX(h.data_reproducao) <= '2021-12-31 23:59:59', 'Usuario ativo', 'Usuario inativo') AS condicao_usuaria
  FROM SpotifyClone.usuarios AS u
  INNER JOIN SpotifyClone.historicos AS h
  ON u.usuario_id = h.usuario_id
