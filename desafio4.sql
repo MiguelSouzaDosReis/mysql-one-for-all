e21fa46b8958a6b924b30d322619068c1f0ed92c
Miguel_Reis <guelsouzar@gmail.com>
2022-02-11 09:09:36 -0300

    desafio 4 corrigindo erro

--- desafio4.sql
+++ desafio4.sql
@@ -1,5 +1,5 @@
  select u.nome as usuario,
- IF (YEAR(MAX(h.data_reproducao)) >= 2021, 'Usuario ativo', 'Usuario inativo') AS 'condicao usuaria'
+ IF (YEAR(MAX(h.data_reproducao)) >= 2021, 'Usuario ativo', 'Usuario inativo') AS condicao_usuaria
  FROM SpotifyClone.usuarios AS u
  INNER JOIN SpotifyClone.historicos AS h
  ON u.usuario_id = h.usuario_id
